600c01a398929cd176c2834d9e8c1da0bab3a507
Anthony-Robichaud <robichaud.anthony@gmail.com>
2026-02-09 17:52:14 -0500

    vision update

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,9 +7,13 @@ package frc.robot;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -17,11 +21,12 @@ import frc.robot.components.Controller;
 import frc.robot.components.DriveFrame;
 import frc.robot.data.StickPosition;
 
-import java.util.List;
+import java.util.*;
 
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.EstimatedRobotPose;
 
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
@@ -31,34 +36,43 @@ public class Robot extends TimedRobot {
 
   private final Controller controller;
   private final DriveFrame driveFrame;
+  private Vision vision;
 
-  public final AprilTagFieldLayout kTagLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
-  public final Transform3d kRobotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
-
-  private final PhotonCamera frontCamera = new PhotonCamera("front-camera");
-  private final PhotonPoseEstimator poseEstimator = new PhotonPoseEstimator(kTagLayout, 
-      PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, kRobotToCam);
+  public static class Constants {
+    public static final String kCameraName = "front-camera";
+    public static final Transform3d kRobotToCam =
+            new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
+    public static final AprilTagFieldLayout kTagLayout =
+            AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+    public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(4, 4, 8);
+    public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 1);
+  }
+  
 
   public Robot() {
     controller = new Controller();
     driveFrame = new DriveFrame();
+
+    vision = new Vision(null);
   }
 
-  int counter = 0;
+  //int counter = 0;
 
   @Override
   public void robotPeriodic() {
-    List<PhotonPipelineResult> result = frontCamera.getAllUnreadResults();
+    //List<PhotonPipelineResult> result = frontCamera.getAllUnreadResults();
     
-    for (PhotonPipelineResult i : result) {
-      poseEstimator.update(i);
-      counter++;
-    }
+    //for (PhotonPipelineResult i : result) {
+    //  poseEstimator.update(i);
+    //  counter++;
+    //}
 
-    if (counter % 20 == 0) {
+    //if (counter % 20 == 0) {
       //SmartDashboard.putData(poseEstimator);
-      counter = 0;
-    }
+    //  counter = 0;
+    //}
+
+    
   }
 
 
